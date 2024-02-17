32c691a808cd15e67bb2c5b905176532bf78f198
steve <steverios_sr@yahoo.com>
2024-02-17 10:08:56 -0600

    settings:fleshing out, apps: added folder and __init__ file

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for config project.
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from decouple import config
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+ADMIN: str = config("ADMIN", cast=str)
+
+urlpatterns: list = [
+    path(ADMIN, admin.site.urls),
+    # path("", include("apps.appNameHere.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
--- config/wsgi.py
+++ config/wsgi.py
@@ -4,13 +4,15 @@ WSGI config for config project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
+from decouple import config
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
+
 
 application = get_wsgi_application()
