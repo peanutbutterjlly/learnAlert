c56830ce04af5b2e7d4b0b02c46ba98b0f12b2e7
peanutbutterjlly <steverios_sr@yahoo.com>
2024-04-08 08:38:35 -0500

    re-changed the admin settings back!

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -8,7 +8,8 @@ from apps.blog.models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    # exclude = ("likes", "dislikes", "shocks", "eyes")
+    # excluding the reactions from the admin - i will not change what people have submitted
+    exclude = ("likes", "dislikes", "shocks", "robots")
     formfield_overrides = {
         models.TextField: {"widget": forms.Textarea(attrs={"rows": 20, "cols": 100})},
         models.CharField: {"widget": forms.TextInput(attrs={"size": 100})},
