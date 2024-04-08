52f892f35d4fab999aa60ef9d3ea200eb622fa21
peanutbutterjlly <steverios_sr@yahoo.com>
2024-04-08 08:26:59 -0500

    temporary admin adjustment

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -8,7 +8,7 @@ from apps.blog.models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    exclude = ("likes", "dislikes", "shocks", "eyes")
+    # exclude = ("likes", "dislikes", "shocks", "eyes")
     formfield_overrides = {
         models.TextField: {"widget": forms.Textarea(attrs={"rows": 20, "cols": 100})},
         models.CharField: {"widget": forms.TextInput(attrs={"size": 100})},
